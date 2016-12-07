71b325536c6f648c9a49f89c9d0a4f249c798d77
huzi <huzion@126.com>
2016-12-07 10:38:26 +0800

    up

--- build/index.js
+++ build/index.js
@@ -8,6 +8,8 @@
 const gulp     = require('gulp')
 const through2 = require('through2');
 const path     = require('path');
+const gutil    = require('gulp-util');
+const color    = gutil.colors;
 
 /*引入构建模块*/
 const js       = require('./lib/javscript');
@@ -32,15 +34,50 @@ var main = {
 
     /*配置config缓存*/
     setting: function(config) {
-        console.log('载入配置信息.......')
-        global.Cache        = global.Cache || function(){return config};
-        global.Cache.config = config;
-        var _config         = global.Cache.config;
+        console.log(color.bgGreen('载入配置信息.......'));
+        var _self = this;
+        var _config = config || _self.noticeConfig();
         _config.dirname     = _config.dirname.replace(/\\/g,'/');
+        _config.separator   = !!_config.separator ? _config.separator : '_';
         _config.srcPath     = path.join(_config.dirname, _config.path.src).replace(/\\/g,'/');
         _config.debugPath   = path.join(_config.dirname, _config.path.debug).replace(/\\/g,'/');
         _config.distPath    = path.join(_config.dirname, _config.path.dist).replace(/\\/g,'/');
+        global.Cache        = {};
+        global.Cache.config = _config;
+    },
 
+    /*提示配置文件信息*/
+    noticeConfig: function() {
+        console.log(
+            color.bgRed('"/config.json" 配置文件不完整或未引用') +
+            color.bgRed('\n"/config.json" 配置文件示例，请依需要自行调整') +
+            color.yellow('\n================================================') +
+            color.yellow('\n{') +
+            color.yellow('\n  "env":"local", ') + color.gray('// local || test || www 环境变量') +
+            color.yellow('\n  "apiEnv": "test", ') + color.gray('// local || test || www 接口环境变量') +
+            color.yellow('\n  "port": "7788", ') + color.gray('// 端口号') +
+            color.yellow('\n  "prefix": "hz", ') + color.gray('// js及css文件名前缀') +
+            color.yellow('\n  "separator": "_", ') + color.gray('// js及css文件名间隔符') +
+            color.yellow('\n  "path": {') +
+            color.yellow('\n    "src": "./src", ') + color.gray('//开发源码目录') +
+            color.yellow('\n    "debug": "./debug", ') + color.gray('//构建生成的测试目录') +
+            color.yellow('\n    "dist": "./dist" ') + color.gray('//构建生成的发布目录') +
+            color.yellow('\n  },') +
+            color.yellow('\n  "appJsPath": "app", ') + color.gray('//js业务逻辑代码文件目录') +
+            color.yellow('\n  "htmlViews": "./views", ') + color.gray('//视图文件生成目录') +
+            color.yellow('\n  "mapPath": "./maps", ') + color.gray('//map文件生成目录') +
+            color.yellow('\n  "coreJs": {') +
+            color.yellow('\n    "mods": [ ') + color.gray('//核心js代码库') +
+            color.yellow('\n      "jquery",') +
+            color.yellow('\n      "ejs"') +
+            color.yellow('\n    ],') +
+            color.yellow('\n    "name": "corelibs",  ') + color.gray('//生成的核心库文件命名') +
+            color.yellow('\n    "src": "./src/js/vender"  ') + color.gray('//核心js代码库文件所在目录') +
+            color.yellow('\n  }') +
+            color.yellow('\n}') +
+            color.yellow('================================================')
+        )
+        return false;
     },
 
     /*开发环境构建*/
